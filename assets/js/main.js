9b5010273b1a5d0c96046d2f4f60cb506526aa2a
diego-cavenati <diegocavenati@gmail.com>
2022-10-05 15:36:16 +0200

    Fix details

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,16 @@
 // JS password generator !!
 
 // Step 1: ask the user his name
-const userName = prompt('Come ti chiami?');
+const userName = prompt("Come ti chiami?");
 
 // Step 2: ask for his surname
-const userSurname = prompt(`Qual'è il tuo cognome?`);
+const userSurname = prompt("Qual'è il tuo cognome?");
 
 // Step 3: ask for his favourite colour
-const userFavoriteColor = prompt(`Qual'è il tuo colore preferito?`);
+const userFavoriteColor = prompt("Qual'è il tuo colore preferito?");
 
 // Step 4: define current year
-const currentYear =  `2022`;
+const currentYear =  "2022";
 // Take last two number
 const yearPassword = currentYear.substring(2);
 
