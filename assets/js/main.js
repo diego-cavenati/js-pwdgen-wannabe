77701a9f388eee7d5e6e39ae54b1cf523c1a196b
diego-cavenati <diegocavenati@gmail.com>
2022-10-05 14:47:31 +0200

    ask user + define current year

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,15 @@
 // JS password generator !!
 
 // Step 1: ask the user his name
-
+const userName = prompt('Come ti chiami?');
 
 // Step 2: ask for his surname
-
+const userSurname = prompt(`Qual'è il tuo cognome?`);
 
 // Step 3: ask for his favourite colour
-
+const userFavoriteColor = prompt(`Qual'è il tuo colore preferito?`);
 
 // Step 4: define current year
-
+const currentYear = 2022;
 
 // Result: write the result on the page
