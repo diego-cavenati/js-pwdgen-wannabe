1ef0ad7dc7e8404b98aeadd7abcc8b9c74388388
diego-cavenati <diegocavenati@gmail.com>
2022-10-05 14:35:09 +0200

    Define workflow JS

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+// JS password generator !!
+
+// Step 1: ask the user his name
+
+
+// Step 2: ask for his surname
+
+
+// Step 3: ask for his favourite colour
+
+
+// Step 4: define current year
+
+
+// Result: write the result on the page
